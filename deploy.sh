3cbe20dd74f6ef1251eadbfc4b196ff371eace3e
jdomene99 <jdomene99@gmail.com>
2023-01-21 21:55:29 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:jdomene99/Pokedex.git main:gh-pages
+git push -f git@github.com:jdomene99/Pagina1React.git main:gh-pages
 
 cd -
